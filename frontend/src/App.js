6c0dff69e4ea70de9bad47adfa7104877b6c995e
Enis <eahmetovic2@etf.unsa.ba>
2019-05-28 19:00:05 +0200

    merge fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,26 +7,6 @@ import '../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min
 
 
 class App extends Component {
-  state = {
-    "token": ""
-  };
-  componentDidMount() {
-    axios.post('/auth',{
-      "username": "Ehvan",
-      "password": "test"
-    })
-      .then(response => {
-        this.setState({token: response.headers.authorization})
-        return response.headers.authorization;
-      })
-      .then(() => axios.get('/nwtUpload/ucenici',{
-        headers: {
-          Authorization: 'Bearer ' + this.state.token 
-        }
-       }))
-       .then(response => console.log(response))
-      .catch(err => console.log(err));
-  }
   render(){
     return (
       <div>
